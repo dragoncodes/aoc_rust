a69c850b90e6a44b41880089d957e75aed88018f
Petar Marinov <dragoncodeseu@gmail.com>
2023-12-09 18:30:58 +0200

    Day-8: Part 2 (not OC idea)

--- 2023/day-8/src/main.rs
+++ 2023/day-8/src/main.rs
@@ -1,5 +1,4 @@
 use itertools::{FoldWhile, Itertools};
-use rayon::prelude::*;
 use std::{collections::HashMap, time::Instant};
 
 fn main() {
@@ -76,12 +75,11 @@ fn solution_part_1(input: &str) -> String {
             acc
         });
 
-    // TODO figure out a rustier way to do this
-    // Maybe with enumarate?
-
-    if let FoldWhile::Done(answer) = instructions.iter().cycle().enumerate().fold_while(
-        String::from("AAA"),
-        |acc, (idx, instruction)| {
+    instructions
+        .iter()
+        .cycle()
+        .enumerate()
+        .fold_while(String::from("AAA"), |acc, (idx, instruction)| {
             let next_node = match instruction {
                 Instruction::Right => &nodes.get(&acc).unwrap().branches.right,
                 Instruction::Left => &nodes.get(&acc).unwrap().branches.left,
@@ -92,16 +90,92 @@ fn solution_part_1(input: &str) -> String {
             } else {
                 FoldWhile::Continue(next_node.to_string())
             }
-        },
-    ) {
-        answer
-    } else {
-        panic!("No answer found");
-    }
+        })
+        .into_inner()
 }
 
 fn solution_part_2(input: &str) -> usize {
-    0
+    // NOTE TO READER:
+    // The solution idea is not original copied from a good guy on reddit
+    // The general purpose solution does not work for the size of the input, unless you've got time
+    // (who does these days??)
+
+    fn gcd(mut a: usize, mut b: usize) -> usize {
+        while b > 0 {
+            let remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+
+        a
+    }
+
+    fn lcm(nums: &[usize]) -> usize {
+        if nums.len() == 1 {
+            return nums[0];
+        }
+        let a = nums[0];
+        let b = lcm(&nums[1..]);
+        a * b / gcd(a, b)
+    }
+
+    let (instructions, nodes) = input.split_once("\n\n").unwrap();
+
+    let instructions = instructions
+        .chars()
+        .map(|c| Instruction::from(c as u8))
+        .collect_vec();
+
+    let nodes = nodes
+        .lines()
+        .map(|line| {
+            let (name, branches) = line.split_once(" = ").unwrap();
+            let (left, right) = branches.split_once(", ").unwrap();
+
+            let left = left.replace("(", "");
+            let right = right.replace(")", "");
+
+            Node {
+                name: name.to_string(),
+                branches: Branches { left, right },
+            }
+        })
+        .fold(HashMap::new(), |mut acc, node| {
+            acc.insert(node.name.clone(), node);
+            acc
+        });
+
+    let starting_nodes = nodes
+        .keys()
+        .filter(|key| key.ends_with("A"))
+        .map(|node_name| nodes.get(node_name).unwrap());
+
+    let cycles = starting_nodes
+        .map(|node| {
+            let mut current_node = node;
+
+            instructions
+                .iter()
+                .cycle()
+                .enumerate()
+                .find_map(|(idx, instruction)| {
+                    current_node = match instruction {
+                        Instruction::Right => nodes.get(&current_node.branches.right).unwrap(),
+
+                        Instruction::Left => nodes.get(&current_node.branches.left).unwrap(),
+                    };
+
+                    if current_node.name.ends_with("Z") {
+                        Some(idx + 1)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap()
+        })
+        .collect_vec();
+
+    lcm(&cycles)
 }
 
 #[cfg(test)]
@@ -139,9 +213,15 @@ ZZZ = (ZZZ, ZZZ)"#
     }
 
     #[test]
-    fn part_1_real() {
-        let input = std::fs::read_to_string("input.txt").unwrap();
+    fn part_2() {
+        let input = r#"
+LLR
 
-        assert_eq!(solution_part_1(&input), "20221");
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"#
+            .trim();
+
+        assert_eq!(solution_part_2(&input), 6);
     }
 }

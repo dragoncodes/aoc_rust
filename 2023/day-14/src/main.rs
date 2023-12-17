1bfd659b81cffaee5b187d15bb59fc4e99224ea4
Petar Marinov <dragoncodeseu@gmail.com>
2023-12-17 18:56:06 +0200

    Day-14: Removed hashmap mutate the grid directly, looks like it's faster

--- 2023/day-14/src/main.rs
+++ 2023/day-14/src/main.rs
@@ -19,11 +19,7 @@ fn main() {
     println!("Part 2: {} ({:?})", part_2_result, part_2_time);
 }
 
-fn can_move_north(
-    stone: (usize, usize),
-    grid: &Vec<Vec<char>>,
-    round_stones: &mut HashMap<(usize, usize), bool>,
-) -> bool {
+fn can_move_north(stone: (usize, usize), grid: &Vec<Vec<char>>) -> bool {
     // Edge of map
     if stone.0 == 0 {
         return false;
@@ -32,46 +28,35 @@ fn can_move_north(
     let new_row = stone.0 - 1;
 
     // The grid has obstacles
-    if grid[new_row][stone.1] == '#' {
-        return false;
-    }
-
-    // There is a round stone in the way
-    if let Some(_) = round_stones.get(&(new_row, stone.1)) {
+    if grid[new_row][stone.1] != '.' {
         return false;
     }
 
     true
 }
 
-fn roll_stone_northwards(
-    stone: (usize, usize),
-    grid: &Vec<Vec<char>>,
-    round_stones: &mut HashMap<(usize, usize), bool>,
-) -> (usize, usize) {
-    if !can_move_north(stone, grid, round_stones) {
+fn roll_stone_northwards(stone: (usize, usize), grid: &mut Vec<Vec<char>>) -> (usize, usize) {
+    if !can_move_north(stone, grid) {
         return stone;
     }
 
     let new_row = stone.0 - 1;
 
-    round_stones.insert((new_row, stone.1), true);
-    round_stones.remove(&stone);
+    grid[new_row][stone.1] = 'O';
+    grid[stone.0][stone.1] = '.';
 
-    return roll_stone_northwards((new_row, stone.1), grid, round_stones);
+    return roll_stone_northwards((new_row, stone.1), grid);
 }
 
-fn get_total_load(grid: &Vec<Vec<char>>, round_stones: &HashMap<(usize, usize), bool>) -> usize {
+fn get_total_load(grid: &Vec<Vec<char>>) -> usize {
     let mut total_load: usize = 0;
 
     for row in 0..grid.len() {
         let mut row_load = 0;
 
         for col in 0..grid[row].len() {
-            if let Some(stone) = round_stones.get(&(row, col)) {
-                if *stone {
-                    row_load += grid.len() - row;
-                }
+            if grid[row][col] == 'O' {
+                row_load += grid.len() - row;
             }
         }
 
@@ -81,35 +66,25 @@ fn get_total_load(grid: &Vec<Vec<char>>, round_stones: &HashMap<(usize, usize), 
     total_load
 }
 
-fn tilt_north(grid: &Vec<Vec<char>>, round_stones: &mut HashMap<(usize, usize), bool>) {
+fn tilt_north(grid: &mut Vec<Vec<char>>) {
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
             if grid[i][j] == 'O' {
-                roll_stone_northwards((i, j), grid, round_stones);
+                roll_stone_northwards((i, j), grid);
             }
         }
     }
 }
 
 fn solution_part_1(input: &str) -> usize {
-    let grid: Vec<Vec<char>> = input
+    let mut grid: Vec<Vec<char>> = input
         .lines()
         .map(|line| line.chars().collect_vec())
         .collect_vec();
 
-    let mut round_stones: HashMap<(usize, usize), bool> = HashMap::new();
+    tilt_north(&mut grid);
 
-    for i in 0..grid.len() {
-        for j in 0..grid[i].len() {
-            if grid[i][j] == 'O' {
-                round_stones.insert((i, j), true);
-            }
-        }
-    }
-
-    tilt_north(&grid, &mut round_stones);
-
-    get_total_load(&grid, &round_stones)
+    get_total_load(&grid)
 }
 
 fn solution_part_2(input: &str) -> i32 {
@@ -137,5 +112,12 @@ O.#..O.#.#
         .trim();
 
         assert_eq!(solution_part_1(input), 136);
+    }
+
+    #[test]
+    fn part1_real() {
+        let input = std::fs::read_to_string("input.txt").unwrap();
+
+        assert_eq!(solution_part_1(&input), 111979);
     }
 }
